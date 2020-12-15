93a7a291e4441aaa82820b92e62cae6a9df0e8ba
Pedro Alves <pedro22carro@gmail.com>
2020-12-15 17:23:35 -0300

    Armazenar nome

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Armazenamento_Do_Nome_Do_Usuario.classes;
+
+namespace Armazenamento_Do_Nome_Do_Usuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o seu nome:");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Digite o seu Sobrenome:");
+            string sobreNome = Console.ReadLine();
+            
+            Pessoa pessoa = new Pessoa(nome,sobreNome);
+        }
+    }
+}
---
+++ classes/Pessoa.cs
@@ -0,0 +1,20 @@
+namespace Armazenamento_Do_Nome_Do_Usuario.classes
+{
+    public class Pessoa
+    {
+        private string sobreNome;
+        public string SobreNome{
+            get{return sobreNome;}
+            set{sobreNome = value;}
+        }
+        private string nome;
+        public string Nome{
+            get{return nome;}
+            set{nome = value;}
+        }
+        public Pessoa(string firstName, string lastName){
+            sobreNome = lastName;
+            nome = firstName;
+        }
+    }
+}
